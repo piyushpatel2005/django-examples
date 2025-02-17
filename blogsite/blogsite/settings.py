a60d313f5b1ecd112549442b9a5f271abfa18c5e
Piyush Patel <piyushpatel2005@gmail.com>
2025-02-17 12:21:33 -0500

    Blog: created sitemap

--- blogsite/blogsite/settings.py
+++ blogsite/blogsite/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+SITE_ID = 1
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
     'django.contrib.staticfiles',
     'taggit',
     'blog.apps.BlogConfig',
