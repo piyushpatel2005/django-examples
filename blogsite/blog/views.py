cd184465b0c06a6a526bdee6a9b96f76420a2f2f
Piyush Patel <piyushpatel2005@gmail.com>
2025-02-16 19:01:51 -0500

    Blog: recommend similar posts

--- blogsite/blog/views.py
+++ blogsite/blog/views.py
@@ -4,6 +4,7 @@ from django.http import Http404
 from django.views.generic import ListView
 from django.views.decorators.http import require_POST
 from django.core.mail import send_mail
+from django.db.models import Count
 from taggit.models import Tag
 from .models import Post
 from .forms import CommentForm, EmailPostForm
@@ -51,13 +52,22 @@ def post_detail(request, year, month, day, post):
     )
     comments = post.comments.filter(active=True)
     form = CommentForm()
+
+    post_tag_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(
+        tags__in=post_tag_ids
+    ).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(
+        same_tags=Count('tags')
+    ).order_by('-same_tags', '-publish')[:4]
     return render (
         request,
         'blog/post/detail.html',
         {
             'post': post,
             'comments': comments,
-            'form': form
+            'form': form,
+            'similar_posts': similar_posts
         }
     )
 
